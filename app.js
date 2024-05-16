a4e1cba8a9ca0741629a3e92841ef692f19ed454
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-05-16 15:40:39 +0800

    Redux basics reducer function with action and payload

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("redux-demo");
+const redux = require("redux");
+
+const initialState = {
+  counter: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + action.payload,
+      };
+    case "DECREMENT":
+      return { counter: state.counter - action.payload };
+    default:
+      return state;
+  }
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const updatedState = store.getState();
+  console.log("updated", updatedState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({
+  type: "INCREMENT",
+  payload: 2,
+});
+store.dispatch({
+  type: "DECREMENT",
+  payload: 1,
+});
